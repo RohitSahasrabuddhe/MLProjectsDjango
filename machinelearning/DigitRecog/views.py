ac1084718f29c315306dda233a2a821ade72ea51
RohitSahasrabuddhe <rohitbhalchandras@gmail.com>
2018-06-26 12:52:12 -0700

    Adding all created files

---
+++ machinelearning/DigitRecog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello, This is my first View!")
+
+def hello(request):
+   return render(request,'./DigitRecog/html/hello.html')
